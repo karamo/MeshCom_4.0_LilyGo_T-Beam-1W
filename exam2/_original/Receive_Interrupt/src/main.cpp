37514f7b83cfb5df8fba70c841126dd0254b3878
karamo <karahoan@drei.at>
2026-01-17 15:52:13 +0100

    corr

--- exam2/_original/Receive_Interrupt/src/main.cpp
+++ exam2/_original/Receive_Interrupt/src/main.cpp
@@ -51,15 +51,9 @@ static String rssi = "0 dBm";
 static String snr = "0 dB";
 static String payload = "0";
 
-// this function is called when a complete packet
-// is received by the module
-// IMPORTANT: this function MUST be 'void' type
-//            and MUST NOT have any arguments!
-void setFlag(void)
-{
-    // we got a packet, set the flag
-    receivedFlag = true;
-}
+// this function is called when a complete packet is received by the module
+// IMPORTANT: this function MUST be 'void' type and MUST NOT have any arguments!
+void setFlag(void) { receivedFlag = true; } // we got a packet, set the flag
 
 uint8_t  display_address = 0x3c;    // It might be 0x3D
 
@@ -67,6 +61,7 @@ U8G2 *disp = NULL;
 #define U8G2_HOR_ALIGN_CENTER(t)    ((disp->getDisplayWidth() -  (disp->getUTF8Width(t))) / 2)
 #define U8G2_HOR_ALIGN_RIGHT(t)     ( disp->getDisplayWidth()  -  disp->getUTF8Width(t))
 
+//=======================================================================================
 bool beginDisplay()
 {
     Wire.beginTransmission(display_address);
@@ -92,6 +87,7 @@ bool beginDisplay()
     return false;
 }
 
+//=======================================================================================
 void drawMain()
 {
     if (disp) {
@@ -136,12 +132,10 @@ void setup()
     #endif
 
     #ifdef RADIO_CTRL
-        /*
-        * 1W and BPF LoRa RX TX Control
-        * CTRL controls the LNA, not the PA.
-        * Only when RX DATA is on, set to 1 to turn on LNA
-        * When TX DATA is on, CTL is set to 0 and LNA is turned off.
-        */
+        // 1W LoRa RX TX Control
+        // RADIO_CTRL controls the LNA, not the PA.
+        // Only when RX DATA is on, set to 1 to turn on LNA
+        // When TX DATA is on, RADIO_CTRL is set to 0 and LNA is turned off.
         pinMode(RADIO_CTRL, OUTPUT);
         digitalWrite(RADIO_CTRL, LOW);
     #endif
@@ -152,62 +146,58 @@ void setup()
 
     beginDisplay();
 
-    // initialize radio with default settings
-    int state = radio.begin();
-
     Serial.printf("[%s]:", RADIO_TYPE_STR);
-    Serial.print(F("Radio Initializing ... "));
+    Serial.printf("Radio Initializing ...\n");
+    int state = radio.begin();    // initialize radio with default settings
     if (state == RADIOLIB_ERR_NONE) {
-        Serial.println(F("success!"));
+        Serial.println("success!");
     } else {
-        Serial.print(F("failed, code "));
-        Serial.println(state);
-        while (true);
+        Serial.printf("failed, code %u >>>>> stopped\n", state);
+        while(true);
     }
 
-    // set the function that will be called
-    // when new packet is received
+    // set the function that will be called when new packet is received
     radio.setPacketReceivedAction(setFlag);
 
     //   Sets carrier frequency.
     //   SX1268/SX1262 : Allowed values are in range from 150.0 to 960.0 MHz.
     if (radio.setFrequency(CONFIG_RADIO_FREQ) == RADIOLIB_ERR_INVALID_FREQUENCY) {
-        Serial.println(F("Selected frequency is invalid for this module!"));
+        Serial.println("Selected frequency is invalid for this module! >>>>> stopped");
         while (true);
     }
 
     //   Sets LoRa link bandwidth.
     //   SX1268/SX1262 : Allowed values are 7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125.0, 250.0 and 500.0 kHz.
     if (radio.setBandwidth(CONFIG_RADIO_BW) == RADIOLIB_ERR_INVALID_BANDWIDTH) {
-        Serial.println(F("Selected bandwidth is invalid for this module!"));
+        Serial.println("Selected bandwidth is invalid for this module! >>>>> stopped");
         while (true);
     }
 
     // Sets LoRa link spreading factor.
     // SX1262        :  Allowed values range from 5 to 12.
     if (radio.setSpreadingFactor(CONFIG_RADIO_SF) == RADIOLIB_ERR_INVALID_SPREADING_FACTOR) {
-        Serial.println(F("Selected spreading factor is invalid for this module!"));
+        Serial.println("Selected spreading factor is invalid for this module! >>>>> stopped");
         while (true);
     }
 
     // Sets LoRa coding rate denominator.
     // SX1278/SX1276/SX1268/SX1262 : Allowed values range from 5 to 8. Only available in LoRa mode.
     if (radio.setCodingRate(CONFIG_RADIO_CR) == RADIOLIB_ERR_INVALID_CODING_RATE) {
-        Serial.println(F("Selected coding rate is invalid for this module!"));
+        Serial.println("Selected coding rate is invalid for this module! >>>>> stopped");
         while (true);
     }
 
     // Sets LoRa sync word.
     // SX1278/SX1276/SX1268/SX1262/SX1280 : Sets LoRa sync word. Only available in LoRa mode.
     if (radio.setSyncWord(CONFIG_RADIO_SW) != RADIOLIB_ERR_NONE) {
-        Serial.println(F("Unable to set sync word!"));
+        Serial.println("Unable to set sync word! >>>>> stopped");
         while (true);
     }
 
     // Sets transmission output power.
     // SX1262        :  Allowed values are in range from -9 to 22 dBm. This method is virtual to allow override from the SX1261 class.
     if (radio.setOutputPower(CONFIG_RADIO_OUTPUT_POWER) == RADIOLIB_ERR_INVALID_OUTPUT_POWER) {
-        Serial.println(F("Selected output power is invalid for this module!"));
+        Serial.println("Selected output power is invalid for this module! >>>>> stopped");
         while (true);
     }
 
@@ -215,52 +205,49 @@ void setup()
     // SX1262/SX1268 : Allowed values range from 45 to 120 mA in 2.5 mA steps and 120 to 240 mA in 10 mA steps.
     // NOTE: set value to 0 to disable overcurrent protection
     if (radio.setCurrentLimit(140) == RADIOLIB_ERR_INVALID_CURRENT_LIMIT) {
-        Serial.println(F("Selected current limit is invalid for this module!"));
+        Serial.println("Selected current limit is invalid for this module! >>>>> stopped");
         while (true);
     }
 
     // Sets preamble length for LoRa or FSK modem.
     // SX1262/SX1268 : Allowed values range from 1 to 65535.
     if (radio.setPreambleLength(16) == RADIOLIB_ERR_INVALID_PREAMBLE_LENGTH) {
-        Serial.println(F("Selected preamble length is invalid for this module!"));
+        Serial.println("Selected preamble length is invalid for this module! >>>>> stopped");
         while (true);
     }
 
     // Enables or disables CRC check of received packets.
     if (radio.setCRC(false) == RADIOLIB_ERR_INVALID_CRC_CONFIGURATION) {
-        Serial.println(F("Selected CRC is invalid for this module!"));
+        Serial.println("Selected CRC is invalid for this module! >>>>> stopped");
         while (true);
     }
 
 
     #ifdef RADIO_CTRL
         Serial.println("Turn on LNA, Enter Rx mode.");
-        // 1W LoRa LNA Control ,set HIGH turn on LNA ,RX Mode
         digitalWrite(RADIO_CTRL, HIGH);
     #endif /*RADIO_CTRL*/
 
     delay(1000);
 
     // start listening for LoRa packets
-    Serial.print(F("Radio Starting to listen ... "));
+    Serial.print("Radio Starting to listen ... ");
     state = radio.startReceive();
     if (state == RADIOLIB_ERR_NONE) {
-        Serial.println(F("success!"));
+        Serial.println("success!");
     } else {
-        Serial.print(F("failed, code "));
-        Serial.println(state);
+        Serial.printf("failed, code %u\n", state);
     }
 
     drawMain();
 }
 
+//=======================================================================================
 void loop()
 {
     // check if the flag is set
     if (receivedFlag) {
-
-        // reset flag
-        receivedFlag = false;
+        receivedFlag = false;   // reset flag
 
         // you can read received data as an Arduino String
         int state = radio.readData(payload);
@@ -279,27 +266,23 @@ void loop()
             drawMain();
 
             // packet was successfully received
-            Serial.println(F("Radio Received packet!"));
+            Serial.println("Radio Received packet!");
 
             // print data of the packet
-            Serial.print(F("Radio Data:\t\t"));
-            Serial.println(payload);
+            Serial.printf("Radio Data:\t\t%s\n", payload.c_str());
 
             // print RSSI (Received Signal Strength Indicator)
-            Serial.print(F("Radio RSSI:\t\t"));
-            Serial.println(rssi);
+            Serial.printf("Radio RSSI:\t\t%s\n", rssi.c_str());
 
             // print SNR (Signal-to-Noise Ratio)
-            Serial.print(F("Radio SNR:\t\t"));
-            Serial.println(snr);
+            Serial.printf("Radio SNR:\t\t%s\n", snr.c_str());
 
         } else if (state == RADIOLIB_ERR_CRC_MISMATCH) {
             // packet was received, but is malformed
-            Serial.println(F("CRC error!"));
+            Serial.println("CRC error!");
         } else {
             // some other error occurred
-            Serial.print(F("failed, code "));
-            Serial.println(state);
+            Serial.printf("failed, code %u\n", state);
         }
 
         // put module back to listen mode
